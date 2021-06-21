3008441b3df76c4e2e72ec9837292ea582bb0fe5
bhaskarnalla <bhaskar.nalla.inbox@gmail.com>
2021-06-21 23:52:42 +0530

    Initial Check-in

--- src/main/java/com/zero/campaign/product/ProductController.java
+++ src/main/java/com/zero/campaign/product/ProductController.java
@@ -1,6 +1,8 @@
 package com.zero.campaign.product;
 
+import com.zero.campaign.product.view.Product;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/product")
 public class ProductController {
 
-    @PostMapping("/product")
-    public void proposeCampaign()
+    @PostMapping("/create")
+    public Product createProduct(@RequestBody Product product)
     {
-
+        // custom logic
+        return product;
     }
 
 }
